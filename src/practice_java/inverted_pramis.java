94acea2b77c3c97ae89490bbd36459fde6981946
pavithra <pavithranaya1108@gmail.com>
2025-02-13 23:02:13 +0530

    feb 13

--- src/practice_java/inverted_pramis.java
+++ src/practice_java/inverted_pramis.java
@@ -4,12 +4,12 @@ public class inverted_pramis {
     public static void main(String[] args) {
         int n=5;
         for(int i=0;i<n;i++){
-            for(int k=0;k<n-i;k++)
+            for(int k=0;k<2*i;k++)
             {
-                System.out.print("*");
-            }
-            for(int j=0;j<2*i-1;j++){
                 System.out.print(" ");
+            }
+            for(int j=0;j<2*(n-i)-1;j++){
+                System.out.print("*");
             }
 
             System.out.println("");
---
+++ src/practice_java/palindrome.java
@@ -0,0 +1,19 @@
+
+package practice_java;
+import java.util.*;
+public class palindrome {
+    public static void main(String args[]){
+        int r,sum=0,temp;
+        int n=12321;
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+}
